c643b5ae899aca980fa587f3ce8f7affe980a1d0
Zyldjan Lanticse <115306019+Zyd8@users.noreply.github.com>
2025-08-08 10:44:20 +0800

    [Update] Configurable JWT expiration

--- project/backend/main.py
+++ project/backend/main.py
@@ -21,7 +21,7 @@ def create_app():
     
     # JWT Configuration
     app.config['JWT_SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = os.getenv('JWT_ACCESS_TOKEN_EXPIRES', timedelta(hours=1))
     app.config['JWT_TOKEN_LOCATION'] = ['headers']
     app.config['JWT_HEADER_NAME'] = 'Authorization'
     app.config['JWT_HEADER_TYPE'] = 'Bearer'
