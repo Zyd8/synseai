d4abc662fb4bedf3fe981be8b08f918aaaae7090
Zyldjan Lanticse <115306019+Zyd8@users.noreply.github.com>
2025-08-18 22:21:01 +0800

    [Fix] Missing import

--- project/backend/models/__init__.py
+++ project/backend/models/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 # Import models here to avoid circular imports
 from .user import User, UserRole  # noqa
 from .company import Company  # noqa
-from .proposal import Proposal  # noqa
+from .proposal import Proposal, ProposalStatus  # noqa
 from .department import Department  # noqa
 from .document import Document  # noqa
 from .synergy import Synergy  # noqa
